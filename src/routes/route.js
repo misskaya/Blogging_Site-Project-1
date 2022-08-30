daa919323cab0302c66942337b67d22ca0e17bce
Kranti Kumar <kkranti1998@gmail.com>
2022-08-30 17:10:24 +0530

    Done Project-1

---
+++ src/routes/route.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+const AuthorController = require('../controllers/authorController');
+const BlogController = require('../controllers/blogController');
+const middleware = require('../middleware/auth');
+
+router.post('/authors', AuthorController.createAuthor);
+
+router.post('/login', AuthorController.loginAuthor);
+
+router.post('/blogs', middleware.authenticate, BlogController.createBlog);
+
+router.get('/blogs', middleware.authenticate, BlogController.getAllBlogs);
+
+router.put('/blogs/:blogId', middleware.authenticate, middleware.authorise,BlogController.updateBlog);
+
+router.delete('/blogs/:blogId',middleware.authenticate,middleware.authorise,BlogController.deleteByParams);
+
+router.delete('/blogs', middleware.authenticate, BlogController.deletedByQuery);
+
+
+
+module.exports = router;
